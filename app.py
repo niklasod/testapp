0270d1af35f69780898c2b8548114252298fc8ca
Niklas Odebo <niklas@odebo.se>
2023-01-15 19:59:00 +0100

    Testing dict

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def test():
-    x = threading.Thread(target=thread_function)
-    x.start()
+    #x = threading.Thread(target=thread_function)
+    #x.start()
    # x.join()
    # response = {"foo:bar", "hello:test"}
-    return """
-    {
-        'test':'foo',
-        'foo':'bar',
-        'name':'jenny',
-        'additional':'true',
-        'canyouseethis':'yes
-    }
-    """
+   response = {
+    "test":"foo"
+   }
+   return response
+#    return """
+#    {
+#        'test':'foo',
+#        'foo':'bar',
+#        'name':'jenny',
+#        'additional':'true',
+#        'canyouseethis':'yes
+#    }
+#    """
 
 def thread_function():
     sleep(1)
